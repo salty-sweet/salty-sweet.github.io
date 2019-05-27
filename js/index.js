ba6e673f725b7aed06fc63fe59c7bb4ce673d8f5
TadaimaSenpai <nightylights@gmail.com>
2019-05-28 01:59:29 +0800

    Minor Changes

--- js/index.js
+++ js/index.js
@@ -1,39 +1,3 @@
-new TypeIt('#introTitle', {
-    startDelay: 1500,
-    speed: 120,
-    lifelike: true,
-    autoStart: false,
-    cursorChar: ""
-    })
-    .type('welcome to the portfolio.')
-    .pause(2500)
-    .delete(25)
-    .pause(950)
-    .options({speed: 90})
-    .type('ＴａｄａｉｍａＳｅｎｐａｉ')
-    .pause(900)
-    .delete(13)
-    .options({speed: 70})
-    .pause(350)
-    .type('ＮｉｇｈｔＬｉｇｈｔｓ')
-    .pause(800)
-    .delete(11)
-    .pause(350)
-    .options({speed: 50})
-    .type('ＳＡＬＴＹＳＷＥＥＴ')
-    .pause(800)
-    .delete(10)
-    .pause(1000)
-    .options({speed: 250})
-    .type('rafael marko.');
-
-new TypeIt('#introSub', {
-  cursorChar: '',
-  speed: 120,
-  lifelike: true
-  })
-  .pause(20000)
-  .type('yeah.').pause(3000).type(" that's me.");
 
 // ------------- VARIABLES ------------- //
 var ticking = false;
